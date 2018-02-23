e43d4a39310b24a9f8fe8d3ea86c4bb38c97f43b
Karl <karl@mae.ee>
2018-02-24 01:30:01 +0200

    Exercise

--- PR04Kindergarten/src/ee/ttu/iti0202/kindergarten/Kindergarten.java
+++ PR04Kindergarten/src/ee/ttu/iti0202/kindergarten/Kindergarten.java
@@ -87,37 +87,6 @@ public class Kindergarten {
         }
         return count;
     }
-
-
-    public static void main(String[] args) {
-
-        Kindergarten kindergarten = new Kindergarten();
-        for (int i = 0; i < 10; i++) {
-            kindergarten.addChild("Jon Snow");
-        }
-        for (int i = 0; i < 10; i++) {
-            kindergarten.addChild("Arya Stark");
-        }
-
-        System.out.println("Children list contains " + kindergarten.getChildrenList().size() + " children."); // Children list contains 20 children.
-        System.out.println("First names " + kindergarten.getAllFirstNameAmounts() + "."); // First names {Arya=10, Jon=10}.
-        System.out.println("With name Jon amount " + kindergarten.getChildrenWithFirstName("Jon") + "."); // With name Jon amount 10.
-        System.out.println("Pairs with matching last names " + kindergarten.getMatchingLastNameAmount() + "."); // Pairs with matching last names 2.
-        System.out.println("Return the fifth child " + kindergarten.returnChild(5) + ".");  // Return the fifth child Jon Snow.
-        System.out.println("List amount has decreased to " + kindergarten.getChildrenList().size() + "."); // List amount has decreased to 19.
-        kindergarten.summerBreak(); // void
-        System.out.println("No more children " + kindergarten.getChildrenList() + "."); // No more children [].
-        System.out.println("Nobody with the name so return " + kindergarten.getChildrenWithFirstName("Arya") + "."); // Nobody with the name so return -1.
-        kindergarten.addChild("Homo naut");
-        kindergarten.addChild("Homo naut");
-        System.out.println("First names " + kindergarten.getAllFirstNameAmounts() + ".");
-        System.out.println("With name Jon amount " + kindergarten.getChildrenWithFirstName("Homo") + "."); // With name Jon amount 10.
-        System.out.println("Pairs with matching last names " + kindergarten.getMatchingLastNameAmount() + "."); // Pairs with matching last names 2.
-        System.out.println("Return the fifth child " + kindergarten.returnChild(1) + ".");  // Return the fifth child Jon Snow.
-        System.out.println("List amount has decreased to " + kindergarten.getChildrenList().size() + "."); // List amount has decreased to 19.
-        kindergarten.summerBreak(); // void
-    }
-
 }
 
 
