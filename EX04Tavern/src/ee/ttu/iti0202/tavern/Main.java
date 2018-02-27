ee8ab6c0bfde58290fed77f34f6898b90126cad1
Karl <karl@mae.ee>
2018-02-27 17:59:20 +0200

    Exercise

--- EX04Tavern/src/ee/ttu/iti0202/tavern/Main.java
+++ EX04Tavern/src/ee/ttu/iti0202/tavern/Main.java
@@ -1,24 +1,6 @@
 package ee.ttu.iti0202.tavern;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-
-        // let's register currencies
-        Currency.add("copper");
-        Currency.add("silver", 10);
-        Currency.add("gold", 100);
-
-
-// to shorten the code
-        Currency c = Currency.get("copper");
-        Currency s = Currency.get("silver");
-        Currency g = Currency.get("gold");
-
-        Purse purse1 = new Purse(new Coin(c), new Coin(c), new Coin(s), new Coin(g)
-        );
-
-        System.out.println(purse1.pay(Price.of(12))); // [1 silver, 1 silver, 1 silver]   should not be 1 gold
     }
 }
