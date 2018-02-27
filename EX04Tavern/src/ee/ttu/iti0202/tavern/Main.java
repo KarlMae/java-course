557c1f96834e2410cf0d3dc8ea3635920472e64f
Karl <karl@mae.ee>
2018-02-27 17:59:01 +0200

    Exercise

--- EX04Tavern/src/ee/ttu/iti0202/tavern/Main.java
+++ EX04Tavern/src/ee/ttu/iti0202/tavern/Main.java
@@ -10,25 +10,15 @@ public class Main {
         Currency.add("silver", 10);
         Currency.add("gold", 100);
 
-        // to shorten the code
 
+// to shorten the code
         Currency c = Currency.get("copper");
         Currency s = Currency.get("silver");
         Currency g = Currency.get("gold");
 
-        Purse purse1 = new Purse(new Coin(c), new Coin(c),
-                new Coin(s), new Coin(s), new Coin(s),
-                new Coin(g), new Coin(g), new Coin(g)
+        Purse purse1 = new Purse(new Coin(c), new Coin(c), new Coin(s), new Coin(g)
         );
 
-        System.out.println(purse1.pay(Price.of(2))); // [1 copper, 1 copper]   should not be 1 silver
-        System.out.println(purse1.pay(Price.of(30))); // [1 silver, 1 silver, 1 silver]   should not be 1 gold
-        System.out.println(purse1.pay(Price.of(200))); // [1 gold, 1 gold]
-        System.out.println(purse1.pay(Price.of(100))); // [1 gold]   should not be [1 gold, 1 gold]
-        System.out.println(purse1.pay(Price.of(1))); // null   purse is empty
-
-
-
-
+        System.out.println(purse1.pay(Price.of(12))); // [1 silver, 1 silver, 1 silver]   should not be 1 gold
     }
 }
