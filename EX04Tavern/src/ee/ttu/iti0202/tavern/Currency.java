5e5ec1cf830747360fd386d0f9bef33390839464
Karl <karl@mae.ee>
2018-02-28 00:04:26 +0200

    Exercise

--- EX04Tavern/src/ee/ttu/iti0202/tavern/Currency.java
+++ EX04Tavern/src/ee/ttu/iti0202/tavern/Currency.java
@@ -1,6 +1,9 @@
 package ee.ttu.iti0202.tavern;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Keeps track of currencies.
