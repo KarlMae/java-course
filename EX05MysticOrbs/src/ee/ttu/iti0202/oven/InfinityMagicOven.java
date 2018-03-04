9585ac33d706afdf8d8caa1573f25f5079f3b0c6
Karl <karl@mae.ee>
2018-03-04 23:50:31 +0200

    Exercise

--- EX05MysticOrbs/src/ee/ttu/iti0202/oven/InfinityMagicOven.java
+++ EX05MysticOrbs/src/ee/ttu/iti0202/oven/InfinityMagicOven.java
@@ -1,5 +1,6 @@
 package ee.ttu.iti0202.oven;
 
+import ee.ttu.iti0202.exceptions.CannotFixException;
 import ee.ttu.iti0202.storage.ResourceStorage;
 
 public class InfinityMagicOven extends MagicOven {
@@ -7,6 +8,10 @@ public class InfinityMagicOven extends MagicOven {
     public InfinityMagicOven(String name, ResourceStorage resourceStorage) {
         super.name = name;
         super.resourceStorage = resourceStorage;
+    }
+
+    public void fix() {
+        throw new CannotFixException(this, CannotFixException.Reason.IS_NOT_BROKEN);
     }
 
     @Override
