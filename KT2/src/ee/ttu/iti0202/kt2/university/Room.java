8e9b81d3ecd38c9fc1a45292904a6932d6bec663
karmae <karmae@ttu.ee>
2018-04-06 15:57:23 +0300

    Test

--- KT2/src/ee/ttu/iti0202/kt2/university/Room.java
+++ KT2/src/ee/ttu/iti0202/kt2/university/Room.java
@@ -1,11 +1,23 @@
 package ee.ttu.iti0202.kt2.university;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
 
     private String name;
+    private List<Integer> bookHour = new ArrayList<>();
 
     public Room(String name) {
         this.name = name;
+    }
+
+    public int bookRoom(int hour) {
+       bookHour.add(int);
+    }
+
+    public boolean getBooked(int hour) {
+        return bookHour.contains(hour);
     }
 
     @Override
