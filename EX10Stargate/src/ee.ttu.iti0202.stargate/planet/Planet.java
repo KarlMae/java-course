aaa55f6c7a32adda8ef8b5fbd9a52b4a68124eb2
Karl <karl@mae.ee>
2018-04-15 15:14:52 +0300

    Goat homework

--- EX10Stargate/src/ee.ttu.iti0202.stargate/planet/Planet.java
+++ EX10Stargate/src/ee.ttu.iti0202.stargate/planet/Planet.java
@@ -1,45 +1,5 @@
 package ee.ttu.iti0202.stargate.planet;
 
-import java.util.List;
-
 public class Planet {
 
-    private String name;
-    private long inhabitants;
-    private boolean stargateAvailable;
-    private boolean dhdhAvailable;
-    private List<String> teamVisited;
-
-    public Planet(String name, long inhabitants, boolean stargateAvailable, boolean dhdAvailable, List<String> teamsVisited) {
-        this.name = name;
-        this.inhabitants = inhabitants;
-        this.stargateAvailable = stargateAvailable;
-        this.dhdhAvailable = dhdAvailable;
-        this.teamVisited = teamsVisited;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public long getInhabitants() {
-        return inhabitants;
-    }
-
-    public boolean isStargateAvailable() {
-        return stargateAvailable;
-    }
-
-    public boolean isDhdhAvailable() {
-        return dhdhAvailable;
-    }
-
-    public List<String> getTeamVisited() {
-        return teamVisited;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
 }
--- EX10Stargate/src/ee.ttu.iti0202.stargate/planet/PlanetBuilder.java
+++ EX10Stargate/src/ee.ttu.iti0202.stargate/planet/PlanetBuilder.java
@@ -1,48 +1,5 @@
 package ee.ttu.iti0202.stargate.planet;
 
-import java.util.List;
-
 public class PlanetBuilder {
-    private String name;
-    private long inhabitants;
-    private boolean stargateAvailable;
-    private boolean dhdAvailable;
-    private List<String> teamsVisited;
 
-    public PlanetBuilder(String name, long inhabitants, boolean stargateAvailable, boolean dhdAvailable, List<String> teamsVisited) {
-        this.name = name;
-        this.inhabitants = inhabitants;
-        this.stargateAvailable = stargateAvailable;
-        this.dhdAvailable = dhdAvailable;
-        this.teamsVisited = teamsVisited;
-    }
-
-    public PlanetBuilder name(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public PlanetBuilder inhabitants(long inhabitants) {
-        this.inhabitants = inhabitants;
-        return this;
-    }
-
-    public PlanetBuilder stargateAvailable(boolean stargateAvailable) {
-        this.stargateAvailable = stargateAvailable;
-        return this;
-    }
-
-    public PlanetBuilder dhdAvailable(boolean dhdAvailable) {
-        this.dhdAvailable = dhdAvailable;
-        return this;
-    }
-
-    public PlanetBuilder teamsVisited(List<String> teamsVisited) {
-        this.teamsVisited = teamsVisited;
-        return this;
-    }
-
-    public Planet build() {
-        return new Planet(name, inhabitants, stargateAvailable, dhdAvailable, teamsVisited);
-    }
 }
