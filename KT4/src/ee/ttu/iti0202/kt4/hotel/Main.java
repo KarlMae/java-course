d66525ddc6706d03abab4f3ead8d891711077656
karmae <karmae@ttu.ee>
2018-05-04 15:47:04 +0300

    KT4

--- KT4/src/ee/ttu/iti0202/kt4/hotel/Main.java
+++ KT4/src/ee/ttu/iti0202/kt4/hotel/Main.java
@@ -14,7 +14,18 @@ public class Main {
 
 
         Hotel hotel = new Hotel();
-        System.out.println("Money in hotel: " + hotel.getMoney());
+        System.out.println("Money in hotel in the beginning: " + hotel.getMoney());
+        System.out.println("\n");
+
+        // Book room, no rooms present
+        System.out.println("Book room, no rooms present");
+        try {
+            hotel.bookRoom(Room.RoomType.PARTY, 2, 4);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        System.out.println("\n");
+
         // Add rooms
         try {
             hotel.addRoom(new RoomBuilder(hotel).setID(1).setRoomType(Room.RoomType.PARTY).createRoom());
@@ -24,7 +35,9 @@ public class Main {
             System.out.println(e.toString());
         }
 
+        System.out.println("Added rooms");
         System.out.println(hotel.getRooms());
+        System.out.println("\n");
 
         // Book room
         try {
@@ -33,16 +46,18 @@ public class Main {
             System.out.println(e.toString());
         }
 
+        System.out.println("Booked room");
         System.out.println(hotel.getRooms());
-        System.out.println("Money in hotel: " + hotel.getMoney());
-
+        System.out.println("Money in hotel: " + hotel.getMoney() + "\n");
 
         // Can't book room
+        System.out.println("Can't book room, already taken");
         try {
             hotel.bookRoom(Room.RoomType.PARTY, 4, 4);
         } catch (Exception e) {
             System.out.println(e.toString());
         }
+        System.out.println("\n");
 
 
         // Add new room same type
@@ -53,7 +68,8 @@ public class Main {
         }
 
 
-        // Now can book room
+        // Now can book room (1 day)
+        System.out.println("Added new room, booking successful");
         try {
             hotel.bookRoom(Room.RoomType.PARTY, 4, 4);
         } catch (Exception e) {
@@ -62,8 +78,10 @@ public class Main {
 
         System.out.println(hotel.getRooms());
         System.out.println("Money in hotel: " + hotel.getMoney());
+        System.out.println("\n");
 
 
+        System.out.println("Trying to add bad rooms");
         // Can't add room ID = 0
         try {
             hotel.addRoom(new RoomBuilder(hotel).setID(0).setRoomType(Room.RoomType.PARTY).createRoom());
